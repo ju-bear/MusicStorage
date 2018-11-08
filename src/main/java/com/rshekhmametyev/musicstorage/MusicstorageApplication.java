61b6e6195aa87e0b504cbc11f4600f52879d4ee5
Rustam Shekhmametyev <rshekhmametyev@gds000986-nb.carina.griddynamics.net>
2018-11-08 13:27:29 +0400

    Initialized empty Spring Boot project

---
+++ src/main/java/com/rshekhmametyev/musicstorage/MusicstorageApplication.java
@@ -0,0 +1,12 @@
+package com.rshekhmametyev.musicstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicstorageApplication.class, args);
+    }
+}
